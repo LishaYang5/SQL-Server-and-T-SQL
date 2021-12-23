7e142c896fb2c83e1a8a2a1662f22caa256335d3
Lucian Qin <jetqin_2008@hotmail.com>
2021-12-23 18:00:34 +1100

    basic select statement

---
+++ Basic_SLECT_Statement.sql
@@ -0,0 +1,14 @@
+/*
+Basic	SELECT	Statement Practice	Problems
+1) Retrieve	all	rows	from	the	HumanResources.Employee	table.		Return	only	the	
+NationalIDNumber	column 
+*/
+SELECT NationalIDNumber
+FROM HumanResources.Employee
+
+/*
+2) Retrieve	all	rows	from	the	HumanResources.Employee	table.		Return	the	
+NationalIDNumber	and	JobTitle	columns.
+*/
+SELECT NationalIDNumber AND JobTitle
+FROM HumanResources.Employee
